a1efd2749fc0c2ed1b36dfd2b2c060150aee8c7b
Ysgramor500 <pedro_aquino_carvalho@hotmail.com>
2022-05-20 17:04:06 -0300

    adding comments to the shell scripts

--- first_stage.sh
+++ first_stage.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
+:'
+This File is supposed to append data of the links corresponding to each movie to the subs_urls file, 
+which will then go on to remove duplicate links retrieved. The links are going to be obtained by using Regex
+on the webpage html of the ytssub website, it will use curl to perform a search of a particular movie, and then
+go on to search for the first link that will appear.
+'
 
 for i in $(tail -8400 all_movies1.txt);
 do
 	echo $i
+	# The "a" variable contains the movie name including the year where it was released
 	a=$(curl -L "https://ytssub.com/search?s=$i"|grep -Eo "https://ytssub.com/movie-subtitles/\S+"|sed -e "s/\"//")
+	
+	:' At times, even though the release date is right the movie can still not be found
+	This might require the use of another variable "c" to try and catch the movie either way
+	In case this results in getting the wrong movie, this will be corrected on the Jupyter Notebook'
+	
 	c=$(echo $i |sed -e "s/[0-9]\+$//")
 	echo $c
 	second_try=$(curl -L "https://ytssub.com/search?s=$c"|grep -Eo "https://ytssub.com/movie-subtitles/\S+"|sed -e "s/\"//")
+
 	b=$(curl -L "$a?lang=english"|grep -Eo "https://ytssub.com/subtitles/\S+"|head -1)
 	e=$(curl -L "$second_try?lang=english"|grep -Eo "https://ytssub.com/subtitles/\S+"|head -1)
 	
 	echo $b
 	echo $e
 	
+	# Finally appending the data to the files	
 	echo $b >> subs_urls.txt;
 	echo $e >> subs_urls.txt;
 done
--- second_script.sh
+++ second_script.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+: 'This file will make use of all the links acquired on the first stage and stored on the subtitles_test.txt file
+to figure out what is the true download link and then go on to download the zip files for each one of those'
+
 for i in $(cat subtitles_test.txt);
 do
 	a=$(echo $i|sed "s/\"//")
 	b=$(curl -L $a| grep -Eo "https://ytssub.com/subtitle-download/\S+"|sed -e "s/\"//")
 	c=$(curl -L $b| grep -Eo "https://ytssub.net/file/data/\S+"|sed -e "s/\"//")
-	get $c
+	wget $c
 done
 
+# Now it's time to finally unzip the files...
 
+for i in $(ls zip); 
+do  
+	unzip $i
+done
 
 
